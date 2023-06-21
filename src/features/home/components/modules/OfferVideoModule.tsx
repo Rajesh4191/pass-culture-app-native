1c7efbe8d25b82c37f908eba0050de548c7e7544
S4andro <131354212+S4andro@users.noreply.github.com>
2023-06-21 13:50:06 +0200

    Revert "(PC-22765) feat(VideoModule): update color to be dynamic (#4924)"
    This reverts commit 23e06ae510be36e83d618db3eda17fa287d450b6.

--- src/features/home/components/modules/OfferVideoModule.tsx
+++ src/features/home/components/modules/OfferVideoModule.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
-import { getTagColor } from 'features/home/components/helpers/getTagColor'
 import { formatDates, getDisplayPrice } from 'libs/parsers'
 import { useCategoryHomeLabelMapping } from 'libs/subcategories'
 import { Offer } from 'shared/offer/types'
@@ -16,10 +15,9 @@ const OFFER_WIDTH = getSpacing(20)
 
 type Props = {
   offer: Offer
-  color: string
 }
 
-export const OfferVideoModule: FunctionComponent<Props> = ({ offer, color }) => {
+export const OfferVideoModule: FunctionComponent<Props> = ({ offer }) => {
   const timestampsInMillis = offer.offer.dates?.map((timestampInSec) => timestampInSec * 1000)
   const labelMapping = useCategoryHomeLabelMapping()
 
@@ -30,7 +28,7 @@ export const OfferVideoModule: FunctionComponent<Props> = ({ offer, color }) => 
           <ImageTile width={OFFER_WIDTH} height={OFFER_HEIGHT} uri={offer.offer.thumbUrl} />
         </OfferImage>
         <OfferInformations>
-          <CategoryText color={color}>{labelMapping[offer.offer.subcategoryId]}</CategoryText>
+          <CategoryText>{labelMapping[offer.offer.subcategoryId]}</CategoryText>
           <TitleText numberOfLines={2}>{offer.offer.name}</TitleText>
           {/* // TODO(PC-22422): Update dates to display only 1 on modal*/}
           <AdditionalInfoText>{formatDates(timestampsInMillis)}</AdditionalInfoText>
@@ -74,10 +72,11 @@ const ArrowOffer = styledButton(Touchable)({
   right: getSpacing(4),
 })
 
-const CategoryText = styled(Typo.Caption)<{ color: string }>(({ color }) => ({
-  color: getTagColor(color),
+const CategoryText = styled(Typo.Caption)({
+  // TODO(PC-22765): update fixed color to dynamic color
+  color: '#B85901FF',
   marginBottom: getSpacing(1),
-}))
+})
 
 const TitleText = styled(Typo.ButtonText)({
   marginBottom: getSpacing(1),
